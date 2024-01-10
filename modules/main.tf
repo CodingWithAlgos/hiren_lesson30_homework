f0b68c4e84dc3de4b6b97c9e6ee913c0ae80e875
CodingWithAlgos <145500042+CodingWithAlgos@users.noreply.github.com>
2024-01-10 13:35:36 +0000

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -13,9 +13,9 @@ terraform {
   }
 }
 
-provider "template" {
+#provider "template" {
   # Configuration options
-}
+#}
 
 data "template_file" "greeting" {
   template = file("${path.module}/greetings.tpl")
